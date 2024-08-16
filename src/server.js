dd300e90a2beaf6c39daef3fea0b2b3ea1e470cb
kuzmavasil74 <98240149+kuzmavasil74@users.noreply.github.com>
2024-08-16 15:13:55 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,4 +1,3 @@
-
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
@@ -31,4 +30,4 @@ export const startServer = () => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
-}
+};
