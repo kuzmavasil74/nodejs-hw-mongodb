1479ae4a65656cc6a831ac174f5e691e2c636356
kuzmavasil74 <98240149+kuzmavasil74@users.noreply.github.com>
2024-08-17 07:31:06 +0200

    some_chanches_1

--- src/validation/updateContactSchema.js
+++ src/validation/updateContactSchema.js
@@ -3,8 +3,10 @@ import Joi from 'joi';
 export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(30),
   email: Joi.string().email(),
-  age: Joi.number().integer().min(6).max(16),
-  gender: Joi.string().valid('male', 'female', 'other'),
-  avgMark: Joi.number().min(2).max(12),
-  onDuty: Joi.boolean(),
+  phoneNumber: Joi.string()
+    .pattern(/^[0-9]+$/)
+    .min(10)
+    .max(15),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid('work', 'home', 'personal'),
 });
